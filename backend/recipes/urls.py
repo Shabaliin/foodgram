aa52cea5edb9eaa667f4d9a6461a0e65f74570d8
Aleksey <96818276+Shabaliin@users.noreply.github.com>
2025-08-27 14:50:13 +0000

    rh

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from .views import router, list_tags, get_tag, list_ingredients, get_ingredient
 
 urlpatterns = [
-    path('', include(router.urls)),  # /recipes/, /recipes/{id}/...
-    path('tags/', list_tags, name='tags-list'),  # /tags/
-    path('tags/<int:id>/', get_tag, name='tag-detail'),  # /tags/{id}/
-    path('ingredients/', list_ingredients, name='ingredients-list'),  # /ingredients/
-    path('ingredients/<int:id>/', get_ingredient, name='ingredient-detail'),  # /ingredients/{id}/
+    path('', include(router.urls)),
+    path('tags/', list_tags, name='tags-list'),
+    path('tags/<int:id>/', get_tag, name='tag-detail'),
+    path('ingredients/', list_ingredients, name='ingredients-list'),
+    path('ingredients/<int:id>/', get_ingredient, name='ingredient-detail'),
 ]
