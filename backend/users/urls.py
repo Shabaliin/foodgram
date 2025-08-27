205d1341fd617784c85fcaccbd092d3eae08001d
Aleksey <96818276+Shabaliin@users.noreply.github.com>
2025-08-27 15:50:10 +0000

    rthrth

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,26 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('users/me/avatar/', views.UserAvatarView.as_view(), name='user-avatar'),
-    path('users/subscriptions/', views.SubscriptionsListView.as_view(), name='subscriptions-list'),
-    path('users/<int:id>/', views.UserDetailView.as_view(), name='user-detail'),
-    path('users/<int:id>/subscribe/', views.SubscribeView.as_view(), name='user-subscribe'),
+    path(
+        'users/me/avatar/',
+        views.UserAvatarView.as_view(),
+        name='user-avatar',
+    ),
+    path(
+        'users/subscriptions/',
+        views.SubscriptionsListView.as_view(),
+        name='subscriptions-list',
+    ),
+    path(
+        'users/<int:id>/',
+        views.UserDetailView.as_view(),
+        name='user-detail',
+    ),
+    path(
+        'users/<int:id>/subscribe/',
+        views.SubscribeView.as_view(),
+        name='user-subscribe',
+    ),
 ]
