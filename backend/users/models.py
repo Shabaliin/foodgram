334c0e76563ac810bc3b5d8e1606f76b5f24d91b
Aleksey <96818276+Shabaliin@users.noreply.github.com>
2025-08-27 15:52:28 +0000

    rthrt

--- backend/users/models.py
+++ backend/users/models.py
@@ -9,7 +9,11 @@ def user_avatar_upload_to(instance: 'User', filename: str) -> str:
 class User(AbstractUser):
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
-    avatar = models.ImageField(upload_to=user_avatar_upload_to, null=True, blank=True)
+    avatar = models.ImageField(
+        upload_to=user_avatar_upload_to,
+        null=True,
+        blank=True,
+    )
 
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
     USERNAME_FIELD = 'email'
@@ -19,8 +23,16 @@ class User(AbstractUser):
 
 
 class Subscription(models.Model):
-    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='subscriptions')
-    author = models.ForeignKey('User', on_delete=models.CASCADE, related_name='subscribers')
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+        related_name='subscriptions',
+    )
+    author = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+        related_name='subscribers',
+    )
 
     class Meta:
         unique_together = ('user', 'author')
