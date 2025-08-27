4202264dfab2d1f2bb8f283f74741992b1e9b967
Aleksey <96818276+Shabaliin@users.noreply.github.com>
2025-08-27 13:13:09 +0000

    erger

--- backend/shortlinks/views.py
+++ backend/shortlinks/views.py
@@ -3,5 +3,5 @@ from recipes.models import RecipeShortLink
 
 
 def short_redirect(request, code: str):
-	link = get_object_or_404(RecipeShortLink, code=code)
-	return redirect(f"/recipes/{link.recipe_id}")
+    link = get_object_or_404(RecipeShortLink, code=code)
+    return redirect(f"/recipes/{link.recipe_id}")
