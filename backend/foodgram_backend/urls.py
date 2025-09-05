6e74cd644280107d997b33ae978ee8c22ca6178c
Aleksey <96818276+Shabaliin@users.noreply.github.com>
2025-09-05 16:23:30 +0000

    wefew

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -8,10 +8,17 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path(
+        'api/schema/redoc/',
+        SpectacularRedocView.as_view(url_name='schema'),
+        name='redoc',
+    ),
     path('api/', include('api.urls')),
     re_path(r'^s/', include('shortlinks.urls')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
