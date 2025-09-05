555f69f836b648cdc6caeca6f25e603d543dabf3
Aleksey <96818276+Shabaliin@users.noreply.github.com>
2025-09-05 16:01:40 +0000

    ergert

--- backend/api/views.py
+++ backend/api/views.py
@@ -209,9 +209,10 @@ class UserViewSet(viewsets.ViewSet):
     )
     def subscriptions(self, request):
         qs = User.objects.filter(
-            id__in=Subscription.objects.filter(
-                user=request.user
-                ).values('author_id')
+            id__in=Subscription
+            .objects
+            .filter(user=request.user)
+            .values('author_id')
         )
         qs = qs.annotate(
             last_pub=Max('recipes__created_at')
